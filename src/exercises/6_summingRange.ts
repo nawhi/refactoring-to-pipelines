84e2837c967e39d765167054b35feabf6c549d23
nawhi <25394965+nawhi@users.noreply.github.com>
2020-10-22 17:36:04 +0100

    Add summing range

---
+++ src/exercises/6_summingRange.ts
@@ -0,0 +1,24 @@
+import { expect } from "chai";
+
+// refactor me to be more functional
+function sumRange(limit: number) {
+  let result = 0;
+  for (let i = 0; i < limit; i++) {
+    result += i;
+  }
+  return result;
+}
+
+describe("Exercise 6 - Summing Range", () => {
+  it("is 0 for limit 0", () => {
+    expect(sumRange(0)).to.eql(0)
+  });
+
+  it("is 6 for limit 4", () => {
+    expect(sumRange(4)).to.eql(6);
+  });
+
+  it("handles big numbers", () => {
+    expect(sumRange(12345678)).to.eql(76207876467003);
+  });
+});
