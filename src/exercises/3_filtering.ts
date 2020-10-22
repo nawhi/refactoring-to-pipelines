e27629ffef83f90b7df0b17ca358427b22299043
nawhi <25394965+nawhi@users.noreply.github.com>
2020-10-22 17:07:48 +0100

    Add Filtering

--- src/exercises/3_filtering.ts
+++ src/exercises/3_filtering.ts
@@ -1,1 +1,18 @@
+import { expect } from "chai";
 
+// refactor me to be more functional
+function removeOddNumbers(numbers: number[]): number[] {
+  const result: number[] = [];
+  for (const number of numbers) {
+    if (number % 2 == 0) {
+      result.push(number);
+    }
+  }
+  return result;
+}
+
+describe("Exercise 3 - Filtering", () => {
+  it("filters out odd numbers", () => {
+    expect(removeOddNumbers([1, 2, 3, 4, 5, 6, 7, 8])).to.eql([2, 4, 6, 8]);
+  });
+});
